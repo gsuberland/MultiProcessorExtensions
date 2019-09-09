099434ad0742cc7cfc87192912894edb35477860
Graham Sutherland <gsuberland@gmail.com>
2019-09-09 03:00:21 +0100

    Added group info tests.

---
+++ MPETests/GroupTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using MultiProcessorExtensions;
+
+namespace MPETests
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void GroupInfoReturnsData()
+        {
+            var groups = MultiProcessorInformation.GetGroupsInfo();
+            Assert.NotEmpty(groups);
+        }
+
+        [Fact]
+        public void ProcessorPackageGroupMaskIsNotEmpty()
+        {
+            var groups = MultiProcessorInformation.GetGroupsInfo();
+            foreach (var group in groups)
+            {
+                Assert.NotEmpty(group.Groups);
+            }
+        }
+
+        [Fact]
+        public void ProcessorPackageGroupMaskIsNotZero()
+        {
+            var groups = MultiProcessorInformation.GetGroupsInfo();
+            foreach (var group in groups)
+            {
+                foreach (var subgroup in group.Groups)
+                {
+                    Assert.True(subgroup.ActiveProcessorMask != UIntPtr.Zero);
+                }
+            }
+        }
+    }
+}
