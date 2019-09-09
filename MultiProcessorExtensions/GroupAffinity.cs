e3c2479994c36cf99ada1afe0f194f172112aae2
Graham Sutherland <gsuberland@gmail.com>
2019-09-09 02:43:07 +0100

    Added IsProcessorActive method to GroupAffinity class.

--- MultiProcessorExtensions/GroupAffinity.cs
+++ MultiProcessorExtensions/GroupAffinity.cs
@@ -20,5 +20,18 @@ namespace MultiProcessorExtensions
             this.Mask = affinity.Mask;
             this.Group = affinity.Group;
         }
+
+        public bool IsProcessorActive(int maskIndex)
+        {
+            // check that the given index is a valid bit offset in the mask
+            int totalMaskBits = (UIntPtr.Size * 8) - 1;
+            if (maskIndex < 0 || maskIndex > totalMaskBits)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maskIndex), $"Mask index was not in the range 0 to {totalMaskBits}.");
+            }
+
+            // return true if the bit is set in the mask
+            return (Mask.ToUInt64() & (1u << maskIndex)) != 0;
+        }
     }
 }
