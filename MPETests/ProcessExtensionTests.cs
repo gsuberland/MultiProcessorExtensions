bcbf56707112e0ec0908a60129269762e2904a12
Graham Sutherland <gsuberland@gmail.com>
2019-09-09 19:44:54 +0100

    Added Process.GetGroupAffinity extension method.

---
+++ MPETests/ProcessExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using Xunit;
+using MultiProcessorExtensions;
+
+namespace MPETests
+{
+    public class ProcessorExtensionTests
+    {
+        [Fact]
+        public void CanGetProcessorGroup()
+        {
+            var proc = Process.GetCurrentProcess();
+            ushort[] groups = proc.GetGroupAffinity();
+            Assert.NotNull(groups);
+            Assert.NotEmpty(groups);
+        }
+    }
+}
--- MultiProcessorExtensions/ProcessExtensions.cs
+++ MultiProcessorExtensions/ProcessExtensions.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
 
 namespace MultiProcessorExtensions
 {
     public static class ProcessExtensions
     {
-        // todo: add extension method that finds the preferred group for a process
+        public static ushort[] GetGroupAffinity(this Process process)
+        {
+            var procHandle = process.Handle;
+
+            ushort arraySize = 0;
+            ushort[] affinities = null;
+
+            bool success = false;
+            const int blown = 100;
+            int fuse = 0;
+
+            /* Attempt to call GetProcessGroupAffinity until the buffer size is sufficient.
+             * This handles a race condition whereby the group count changes between an initial call to GetProcessGroupAffinity
+             * to get the required buffer length, and a second call to fill the buffer.
+             */
+            do
+            {
+                affinities = new ushort[arraySize];
+                if (!NativeMethods.GetProcessGroupAffinity(procHandle, ref arraySize, affinities))
+                {
+                    var lastError = Marshal.GetLastWin32Error();
+                    if (lastError != NativeMethods.ERROR_INSUFFICIENT_BUFFER)
+                    {
+                        throw new InvalidOperationException($"GetProcessGroupAffinity call failed. GetLastError: {lastError}");
+                    }
+                    if (++fuse == blown)
+                    {
+                        throw new TimeoutException("GetProcessGroupAffinity call did not return a valid buffer size.");
+                    }
+                }
+                else
+                {
+                    success = true;
+                }
+            }
+            while (!success);
+            return affinities;
+        }
     }
 }
